6d98e7a2776e1c47650cbb65b02700b76e4e71d8
VPateNa <Nandita.Patel@pearson.com>
2020-03-23 12:43:36 +0530

    Updates

---
+++ precache-manifest.e1a1ab334a1d122c99a149040ebaeb4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e383346048211866aa78b508c63120",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6b5e0543b5e511cb612f",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6b5e0543b5e511cb612f",
+    "url": "/contactmanager/static/js/2.17e61569.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/contactmanager/static/js/2.17e61569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6733bb7d24046a73b9f9",
+    "url": "/contactmanager/static/js/main.34f9b549.chunk.js"
+  },
+  {
+    "revision": "22a1eb616042739cf5e8",
+    "url": "/contactmanager/static/js/runtime-main.900c19c8.js"
+  }
+]);
